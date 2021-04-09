6117b7f792e6daa14a8c05f976233217d5b28e4d
cjh99 <cjharr6980@ung.edu>
2021-04-09 17:40:39 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/cjh_8_bitentrees/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cjh_8_bitentrees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
